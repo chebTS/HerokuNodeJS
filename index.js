8c1af1244ac0abc117fbc8bd20b037288bd8ce56
chebts <chebTS@gmail.com>
2016-07-09 17:07:27 +0300

    test

--- index.js
+++ index.js
@@ -9,9 +9,22 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+var request = require('request'); // npm install request
+app.post('^*$', function(req, res) {
+	res.redirect(307, 'http://skypebot.ekreative.com/receive' );
+    // request({ url: 'http://remoteserver.com' + req.path, headers: req.headers, body: req.body },
+    //  		function(err, remoteResponse, remoteBody) {
+    //     	if (err) { return res.status(500).end('Error'); 
+    //     	}
+    //     res.writeHead(...); // copy all headers from remoteResponse
+    //     res.end(remoteBody);
+    // });
 });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
