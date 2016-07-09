605e122cd068e1e6d4026fa8bdf73a611fae7075
chebts <chebTS@gmail.com>
2016-07-09 17:14:52 +0300

    test

--- index.js
+++ index.js
@@ -13,8 +13,38 @@ app.set('view engine', 'ejs');
  //   response.render('pages/index');
  // });
 
- http.get('/',function(req,res){  
- 	 res.redirect('http://stackoverflow.com/questions/11355366/nodejs-redirect-url')
+ http.get('/',function(requese,res){  
+ 
+			var querystring = require('querystring');
+			var http = require('http');
+
+			var data = querystring.stringify({
+			      username: yourUsernameValue,
+			      password: yourPasswordValue
+			    });
+
+			var options = {
+			    host: 'http://skypebot.ekreative.com/receive',
+			    port: 80,
+			    path: '/',
+			    method: 'POST',
+			    headers: {
+			        'Content-Type': 'application/x-www-form-urlencoded',
+			        'Content-Length': Buffer.byteLength(data)
+			    }
+			};
+
+			var req = http.request(options, function(res) {
+			    res.setEncoding('utf8');
+			    res.on('data', function (chunk) {
+			        console.log("body: " + chunk);
+			    });
+			});
+
+			req.write(data);
+			req.end();
+
+
      //res.redirect('http://skypebot.ekreative.com/receive')
  })
 
