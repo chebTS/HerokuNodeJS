6ab3cea668b6aefdfffca84b11f2146e65f1d18d
chebts <chebTS@gmail.com>
2016-07-09 17:18:34 +0300

    test

--- index.js
+++ index.js
@@ -9,44 +9,16 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
- // app.get('/', function(request, response) {
- //   response.render('pages/index');
- // });
+  app.get('/', function(request, response) {
+    //response.render('pages/index');
+    response.redirect(301, 'http://ekreative.com');
+  });
 
- http.get('/',function(requese,res){  
+ // http.get('/',function(requese,res){  
  
-			var querystring = require('querystring');
-			var http = require('http');
 
-			var data = querystring.stringify({
-			      username: yourUsernameValue,
-			      password: yourPasswordValue
-			    });
-
-			var options = {
-			    host: 'http://skypebot.ekreative.com/receive',
-			    port: 80,
-			    path: '/',
-			    method: 'POST',
-			    headers: {
-			        'Content-Type': 'application/x-www-form-urlencoded',
-			        'Content-Length': Buffer.byteLength(data)
-			    }
-			};
-
-			var req = http.request(options, function(res) {
-			    res.setEncoding('utf8');
-			    res.on('data', function (chunk) {
-			        console.log("body: " + chunk);
-			    });
-			});
-
-			req.write(data);
-			req.end();
-
-
-     //res.redirect('http://skypebot.ekreative.com/receive')
- })
+ //     //res.redirect('http://skypebot.ekreative.com/receive')
+ // })
 
 
 
