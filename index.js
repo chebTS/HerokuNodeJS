69ecfbfc77cddc5ce4616de951fe952460aaa27c
chebts <chebTS@gmail.com>
2016-07-09 17:11:04 +0300

    test

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
+ app.get('/', function(request, response) {
+   response.render('pages/index');
+ });
 
-http.get('/',function(req,res){  
-    res.redirect('http://skypebot.ekreative.com/receive')
-})
+// http.get('/',function(req,res){  
+//     res.redirect('http://skypebot.ekreative.com/receive')
+// })
 
 
 
